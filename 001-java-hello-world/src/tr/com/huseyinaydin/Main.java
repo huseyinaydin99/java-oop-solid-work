d33a23b39cd55918194a301de779a79c37da4505
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-10-20 13:37:28 +0300

    🌟💻🔥 Java: Platform Bağımsızlığı, Bytecode & JVM Mekanizması 👊⚡📦
    Merhabalar, ben Javayım. Ben, Sun Microsystems’in enteresan mühendisleri tarafından geliştirildim; sıradan bir dil değilim. Hem derlenebilen hem yorumlanabilen, yüksek performanslı ve platform bağımsız bir programlama ortamıyım, çünkü kodlarım derlendiğinde bytecode adı verilen evrensel bir form alır  ve JVM sayesinde farklı işletim sistemlerinde ve donanımlarda aynı şekilde çalışır. Kodlarım paketlendiğinde ise class dosyaları haline gelir ve JAR veya WAR dosyaları olarak dağıtılabilir, bu da beni hem taşınabilir hem de büyük sistemlerde kullanılabilir kılar. Benim tarihçem de bir serüven: Önce OAK, sonra Green, ve sonunda Java adını aldım; 1995 yılında dünyaya merhaba dedim. Dünyadaki milyonlarca geliştirici, kurumsal sistem, mobil uygulama ve gömülü cihaz benimle şekillendi; ben sadece bir programlama dili değilim, aynı zamanda teknoloji ekosisteminin temel taşlarından biriyim. Hüseyin AYDIN ile aramızda ay farkı olabilir ama ben sürekli güncellenen, ve Hüseyin gibi eski kafa olmayan modern yazılım dünyasının kalbiyim. Ana amacım net: Geliştiricilere “bir kez yaz, her yerde çalıştır” özgürlüğü sunmak, güvenli, ölçeklenebilir ve sürdürülebilir yazılımlar oluşturmayı mümkün kılmak. Ben, öğrenmekten korkmayanların, yeniliğe açık olanların, büyük kurumların ve büyük hayaller kuranların diliyim. 🌟💻🔥

---
+++ 001-java-hello-world/src/tr/com/huseyinaydin/Main.java
@@ -0,0 +1,65 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java Generics
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("""
+            Merhabalar, ben Javayım. Ben, Sun Microsystems’in vizyoner mühendisleri tarafından geliştirildim;
+            sıradan bir dil değilim. Hem derlenebilen hem yorumlanabilen, yüksek performanslı ve platform bağımsız
+            bir programlama ortamıyım, çünkü kodlarım derlendiğinde bytecode adı verilen evrensel bir form alır
+            ve JVM sayesinde farklı işletim sistemlerinde ve donanımlarda aynı şekilde çalışır. Kodlarım
+            paketlendiğinde ise class dosyaları haline gelir ve JAR veya WAR dosyaları olarak dağıtılabilir,
+            bu da beni hem taşınabilir hem de büyük sistemlerde kullanılabilir kılar.
+            Benim tarihçem de bir serüven: Önce OAK, sonra Green, ve sonunda Java adını aldım;
+            1995 yılında dünyaya merhaba dedim. Dünyadaki milyonlarca geliştirici, kurumsal sistem,
+            mobil uygulama ve gömülü cihaz benimle şekillendi; ben sadece bir programlama dili değilim,
+            aynı zamanda teknoloji ekosisteminin temel taşlarından biriyim. 
+            Hüseyin AYDIN ile aramızda ay farkı olabilir ama ben sürekli güncellenen, 
+            ve Hüseyin gibi eski kafa olmayan modern yazılım dünyasının kalbiyim.
+            Ana amacım net: Geliştiricilere “bir kez yaz, her yerde çalıştır” özgürlüğü sunmak, güvenli,
+            ölçeklenebilir ve sürdürülebilir yazılımlar oluşturmayı mümkün kılmak. 
+            Ben, öğrenmekten korkmayanların, yeniliğe açık olanların, büyük kurumların ve büyük hayaller kuranların diliyim. 🌟💻🔥
+            """);
+
+        //Java 25 ile ama biz 24'deyiz şu an olsun.
+        /*IO.println("""
+                Her sene bu konu gündeme geliyor.
+                
+                “Java güncelliğini yitirdi.”
+                "Java çok ayrıntılı."
+                "Artık kimse Java kullanmıyor."
+                
+                Ve her yıl gerçek bunun tam tersini kanıtlıyor.
+                
+                ✅ Google hala Java kullanıyor.
+                ✅ Netflix hala Java kullanıyor.
+                ✅ AWS hala Java kullanıyor.
+                ✅ Fortune 500 şirketleri, büyük ve kritik öneme sahip sistemleri desteklemek için Java'ya güveniyor.
+                
+                Yakın zamanda, yeni diller hakkındaki bitmek bilmeyen abartılara rağmen Java popülerlik endekslerinde (TIOBE, Stack Overflow) en üst sıralarda yer almaya devam ediyor.
+                Doğrusu?
+                
+                Programlama dilleri, sadece bir eğitim kampında birkaç kişinin onları korkutucu bulması yüzünden "ölmez".
+                
+                Java mükemmel değildir, hiçbir dil mükemmel değildir.
+                
+                Ancak birçok işletmenin bir gecede terk etmeyeceği bir ekosistemi, araçları ve onlarca yıllık savaşta test edilmiş kütüphaneleri var.
+                
+                Eğer hangi dili öğreneceğinize karar veriyorsanız, benim bakış açım şu şekilde:
+                
+                💡 İşe uygun aracı seçin, trendi değil.
+                💡 Temelleri anlamaya odaklanın. Sözdizimi her zaman öğrenilebilir.
+                💡 Önce mühendis ol, sonra dil tutkunu.
+                """);*/
+	}
+}
