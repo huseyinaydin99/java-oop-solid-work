d33e1eae149fe7a92fea7dda11a0aa5996cc85c9
Hüseyin Aydın <huseyinaydin99@gmail.com>
2026-07-21 17:36:43 +0300

    ...
    ...

--- 018_java-collections-framework-jcf/src/main/java/tr/com/huseyinaydin/_06/MyQueue.java
+++ 018_java-collections-framework-jcf/src/main/java/tr/com/huseyinaydin/_06/MyQueue.java
@@ -1,5 +1,6 @@
 package tr.com.huseyinaydin._06;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -29,5 +30,39 @@ public class MyQueue {
         studentList2.add("Selami");
         studentList2.add("Behlül");
         System.out.println(studentList2);
+
+        Queue<String> studentList3 = new PriorityQueue<>(Comparator.reverseOrder());
+        studentList3.add("Selami");
+        studentList3.add("Behlül");
+        studentList3.add("Fahrettin");
+        // studentList3.add(null); // Olmaz!!
+        studentList3.add("Veli");
+        studentList3.add("Selami");
+        studentList3.add("Behlül");
+        System.out.println(studentList3);
+
+        System.out.println("----------------------");
+
+        while (!studentList3.isEmpty()) {
+            System.out.println(studentList3.poll());
+        }
+
+        Queue<String> studentList4 = new PriorityQueue<>(Comparator.naturalOrder());
+        studentList4.add("Selami");
+        studentList4.add("Behlül");
+        studentList4.add("Fahrettin");
+        // studentList4.add(null); // Olmaz!!
+        studentList4.add("Veli");
+        studentList4.add("Selami");
+        studentList4.add("Behlül");
+        System.out.println(studentList4);
+
+        System.out.println("----------------------");
+
+        while (!studentList4.isEmpty()) {
+            System.out.println(studentList4.poll());
+        }
+
+        if(studentList4.isEmpty()) System.out.println("kuyruk boştur boşalmıştır");
     }
 }
